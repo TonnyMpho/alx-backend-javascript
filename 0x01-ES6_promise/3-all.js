e2468337427945541a57c540d6c7b5decd31b74b
TonnyMpho <tonnympho1@gmail.com>
2024-01-03 12:44:18 -0800

    ES6 Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photo, user] = results;
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     }).catch(() => {
       console.log('Signup system offline');
     });
