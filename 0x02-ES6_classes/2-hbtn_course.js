3ca7e91175273335bd4bf8c9ce24aa031f6a5667
TonnyMpho <tonnympho1@gmail.com>
2024-01-04 13:11:40 -0800

    ES6 classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(len) {
+    if (typeof (len) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = len;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(studentsList) {
+    if (!Array.isArray(studentsList)) {
+      throw TypeError('students must be a array');
+    }
+    this._students = studentsList;
+  }
+}
