0d267be9ab5f3802b35e32d8792b2eab35c2db66
TonnyMpho <tonnympho1@gmail.com>
2024-01-04 13:53:37 -0800

    ES6 classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(curr) {
+    this._currency = curr;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
