71666882d8982859ff3c64747ee4fe7372e1c926
TonnyMpho <tonnympho1@gmail.com>
2024-01-09 09:27:05 -0800

    ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,8 @@
+/*
+ * function named hasValuesFromArray that returns a boolean
+ * if all the elements in the array exist within the set
+ */
+
+export default function hasValuesFromArray(set, array) {
+  return array.every((element) => set.has(element));
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+/*
+ * function named cleanSet that returns a string of all the set values
+ * that start with a specific string (startString).
+ *
+ * It accepts two arguments: a set (Set) and a startString (String).
+ * When a value starts with startString - appends the rest of the string.
+ * The string contains all the values of the set separated by -
+ */
+
+export default function cleanSet(set, startString) {
+  if (typeof startString === 'string' && startString) {
+    const array = Array.from(set).filter((val) => val.startsWith(startString));
+
+    return array.map((val) => val.slice(startString.length)).join('-');
+  }
+  return '';
+}
