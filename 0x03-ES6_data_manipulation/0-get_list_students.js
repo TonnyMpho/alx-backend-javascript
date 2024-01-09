dd58a6741762b8efc7331e565a4b7ce06de90c64
TonnyMpho <tonnympho1@gmail.com>
2024-01-09 01:27:03 -0800

    ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+// function named getListStudents that returns an array of objects
+// Each object should have three attributes: id (Number), firstName (String), and location (String).
+
+export default function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      lovation: 'San Francisco',
+    },
+  ];
+}
