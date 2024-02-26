525a6a6c7475ade9ffc52ee9854bcb60cb945d61
TonnyMpho <tonnympho1@gmail.com>
2024-02-26 17:23:46 +0000

    NodeJS Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const input = data.toString();
-  console.log(`Your name is: ${input}`);
+  process.stdout.write(`Your name is: ${input}`);
   process.exit();
 });
 
