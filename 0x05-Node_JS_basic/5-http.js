4c2665aee4b4b8286eabac3f288f5fa8c6a1f313
TonnyMpho <tonnympho1@gmail.com>
2024-02-27 18:29:40 +0000

    NodeJS Basics

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -45,7 +45,7 @@ const app = http.createServer(async (req, res) => {
 
       res.end(`This is the list of our students\n${studentsInfo}`);
     } catch (error) {
-      res.end(error.message);
+      res.end('Cannot load the database');
     }
   } else {
     res.end('Not Found');
