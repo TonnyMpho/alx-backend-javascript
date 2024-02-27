8fdbeabcf96978b12f06367521af008805049afb
TonnyMpho <tonnympho1@gmail.com>
2024-02-27 18:14:33 +0000

    NodeJS Basics

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const databasePath = process.argv[2];
+    const studentsInfo = await countStudents(databasePath);
+
+    res.send(`This is the list of our students:\n${studentsInfo}`);
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+});
+
+app.listen(PORT)
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter(Boolean); // Remove empty lines
+
+    let totalStudents = 0;
+    const fieldCounts = {};
+
+    for (const line of lines.slice(1)) {
+      const [, , , field] = line.split(','); // Destructuring to ignore unused variables
+      if (field) {
+        totalStudents += 1;
+        fieldCounts[field] = (fieldCounts[field] || 0) + 1;
+      }
+    }
+
+    let result = `Number of students: ${totalStudents}\n`;
+
+    for (const field of Object.keys(fieldCounts)) {
+      const studentList = lines.slice(1)
+        .filter((line) => line.split(',')[3] === field)
+        .map((line) => line.split(',')[0])
+        .join(', ');
+      result += `Number of students in ${field}: ${fieldCounts[field]}. List: ${studentList}\n`;
+    }
+
+    return result;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = app;
