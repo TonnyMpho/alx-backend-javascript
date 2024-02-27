15ad67a9270851281ba06e52f2a73dd0af0ea0fe
TonnyMpho <tonnympho1@gmail.com>
2024-02-27 17:26:36 +0000

    NodeJS Basics

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,36 +1,38 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter(Boolean); // Remove empty lines
-
-    let totalStudents = 0;
-    const fieldCounts = {};
-
-    for (const line of lines.slice(1)) {
-      const [, , , field] = line.split(',');
-      if (field) {
-        totalStudents += 1;
-        fieldCounts[field] = (fieldCounts[field] || 0) + 1;
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-    }
 
-    let result = `Number of students: ${totalStudents}\n`;
+      const lines = data.split('\n').filter(Boolean); // Remove empty lines
 
-    for (const field of Object.keys(fieldCounts)) {
-      const studentList = lines.slice(1)
-        .filter((line) => line.split(',')[3] === field)
-        .map((line) => line.split(',')[0])
-        .join(', ');
-      result += `Number of students in ${field}: ${fieldCounts[field]}. List: ${studentList}\n`;
-    }
+      let totalStudents = 0;
+      const fieldCounts = {};
 
-    return result;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+      for (const line of lines.slice(1)) {
+        const [, , , field] = line.split(','); // Destructuring to ignore unused variables
+        if (field) {
+          totalStudents += 1;
+          fieldCounts[field] = (fieldCounts[field] || 0) + 1;
+        }
+      }
+
+      results = `Number of students: ${totalStudents}\n`;
+      for (const field of Object.keys(fieldCounts)) {
+        const studentList = lines.slice(1)
+          .filter((line) => line.split(',')[3] === field)
+          .map((line) => line.split(',')[0])
+        results += `Number of students in ${field}: ${fieldCounts[field]}. List: ${studentList}`;
+      }
+
+      resolve(results);
+    });
+  });
 }
 
 const app = http.createServer(async (req, res) => {
