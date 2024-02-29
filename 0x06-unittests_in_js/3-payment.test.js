dbbddde8e6415045a325f79fcf420981e13cfd82
TonnyMpho <tonnympho1@gmail.com>
2024-02-29 20:29:40 +0000

    Unittests in JS

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,18 @@
+// 3-payment.test.js
+const sinon = require('sinon');
+const assert = require('assert');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with the correct arguments and log the result', function () {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    assert(calculateNumberSpy.calledWith('SUM', 100, 20));
+
+    calculateNumberSpy.restore();
+  });
+});
+
