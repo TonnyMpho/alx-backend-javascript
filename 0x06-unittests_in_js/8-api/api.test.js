0564e82d082e8c20f57d51a0dc6e954bec048319
TonnyMpho <tonnympho1@gmail.com>
2024-02-29 20:52:34 +0000

    Unittests in JS

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,28 @@
+// api.test.js
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api');
+
+describe('Index page', function () {
+  it('Correct status code?', function (done) {
+    request('http://localhost:7865', function (error, response) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', function (done) {
+    done();
+  });
+
+  after(function () {
+    app.close();
+  });
+});
