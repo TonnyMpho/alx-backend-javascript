7a082e56cc6dcc1c8a320a2f1326785341e3e326
TonnyMpho <tonnympho1@gmail.com>
2024-02-29 10:22:21 +0000

    Unittests in JS

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,19 @@ const assert = require('assert')
 const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', () => {
-	it('return the rounded sum of two integers', () => {
-		assert.equal(calculateNumber(1, 3), 4);
-	});
+  it('return the rounded sum of two integers', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
 
-	it('return the rounded sum of an integer and a float', () => {
-		assert.equal(calculateNumber(1, 3.7), 5);
-	});
+  it('return the rounded sum of an integer and a float', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
 
-	it('return the rounded sum of two floats', () => {
-		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-	});
+  it('return the rounded sum of two floats', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
 
-	it('return the rounded sum of two floats', () => {
-		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-	});
+  it('return the rounded sum of two floats', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
