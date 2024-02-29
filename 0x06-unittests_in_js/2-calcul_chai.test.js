9c6774f9a2b47fa9c5a23583722efb09de79826d
TonnyMpho <tonnympho1@gmail.com>
2024-02-29 21:10:45 +0000

    Unittests in JS

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -12,7 +12,7 @@ describe('calculateNumber', () => {
   });
 
   it('should return the result of dividing two rounded numbers when type is DIVIDE', () => {
-    expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(0.405);
+    expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(0.5);
   });
 
   it('should return "Error" when trying to divide by 0', () => {
